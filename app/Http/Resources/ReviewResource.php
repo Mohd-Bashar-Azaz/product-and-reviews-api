5f591ce25cb21d3a210c3414e05cc9a15f75723e
Mohd-Bashar-Azaz <145273794+Mohd-Bashar-Azaz@users.noreply.github.com>
2024-06-24 11:11:33 +0530

    Update ReviewResource.php

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -18,15 +18,7 @@ class ReviewResource extends JsonResource
             'customer' => $this->customer,
             'body' => $this->review,
             'star' => $this->star,
-            // 'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
-            // 'discount' => $this->discount,
-            // 'totalPrice' =>round((1-($this->discount/100))* $this->price),
-
-            // 'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No Rating Yet',
-            // 'href' => [
-            //     'reviews' => route('reviews.index', $this->id)
-
-            // ]
+            
         ];
     }
 }
